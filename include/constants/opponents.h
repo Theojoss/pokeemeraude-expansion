f2e1986cb95988a11a768343b1b246022c284e68
theojoss <tjosserand@luccasoftware.com>
2026-07-26 19:20:48 +0200

    Remove Ziggy and Falinksite sold

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -859,7 +859,6 @@
 #define TRAINER_LEAF                        852
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
-#define TRAINER_ZIGGY                       855
 #define TRAINER_THEOJOSS                    856
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
